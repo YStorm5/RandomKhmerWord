d590413c4ed6cc94414634dd43dcce7fc26263f6
sopanha.khun <sopanha.khun@FCHQ-BI-350>
2023-09-27 15:40:56 +0700

    fix issue relate to reading file

--- khmer_random.ts
+++ khmer_random.ts
@@ -1,16 +1,17 @@
 import { readLines } from "https://deno.land/std@0.202.0/io/mod.ts";
-const file = await Deno.open("km_KH.dic", { read: true });
 
 function getRandomNum() {
   const MAX = 75100;
   return Math.floor(Math.random() * (MAX - 1)) + 1;
 }
 async function getRandomKhmerWord(input: number) {
+  const file = await Deno.open("km_KH.dic", { read: true });
   let startIndex = 1;
   let _word;
   for await (const line of readLines(file)) {
     if (startIndex == input) {
       _word = line;
+      file.close();
       break;
     }
     startIndex++;
@@ -21,10 +22,16 @@ async function getRandomKhmerWord(input: number) {
 Deno.serve(async (req) => {
   const word = await getRandomKhmerWord(getRandomNum());
   const body = JSON.stringify({ word: word });
-  return new Response(body, {
-    status: 200,
-    headers: {
-      "content-type": "application/json; charset=utf-8",
-    },
-  });
+  if (word != null) {
+    return new Response(body, {
+      status: 200,
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+      },
+    });
+  } else {
+    return new Response("Run out of word please try again :(", {
+      status: 404,
+    });
+  }
 });
